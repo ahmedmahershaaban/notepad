b992a8d20283f98bb7a2cc5bf8310bdf3d29733b
ahmedmahershaaban <ahmedmaher13579@outlook.com>
2020-07-17 01:08:05 +0200

    fixing logout

--- config/logout.php
+++ config/logout.php
@@ -2,7 +2,7 @@
 session_start();
 
 
-if(isset($_SESSION['user_id']) && $_GET['logout'] == 1){
+if($_GET['logout'] == 1){
     session_destroy();
     setcookie("rememberme", "", time()-604800,'/');
     header("location: http://notepad-github.gearhostpreview.com/MyNote.php");

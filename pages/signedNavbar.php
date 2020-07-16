dd8757c067830d0f01356163ef1093a7b1d7fd1b
ahmedmahershaaban <ahmedmaher13579@outlook.com>
2020-07-16 06:35:03 +0200

    Fixing the navbar active

--- pages/signedNavbar.php
+++ pages/signedNavbar.php
@@ -6,10 +6,30 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav mr-auto">
-      <a class="nav-item nav-link active" href="MyNote.php">My Notes <span class="sr-only">(current)</span></a>
+       <?php  
+                $current = $_SERVER["PHP_SELF"];
+                if (strpos($current, 'MyNote.php') !== false){
+                    echo ' <a class="nav-item nav-link active" href="MyNote.php">My Notes <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="AboutUs.php">About Us</a>
       <a class="nav-item nav-link" href="ContactUs.php">Contact Us</a>
-      <a class="nav-item nav-link" href="Profile.php">Profile</a>
+      <a class="nav-item nav-link" href="Profile.php">Profile</a>';
+                }else if(strpos($current, 'AboutUs.php') !== false){
+                    echo '<a class="nav-item nav-link" href="MyNote.php">My Notes</a>
+      <a class="nav-item nav-link active" href="AboutUs.php">About Us <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="ContactUs.php">Contact Us</a>
+      <a class="nav-item nav-link" href="Profile.php">Profile</a>';
+                }else if(strpos($current, 'ContactUs.php') !== false){
+                    echo '<a class="nav-item nav-link" href="MyNote.php">My Notes</a>
+      <a class="nav-item nav-link" href="AboutUs.php">About Us</a>
+      <a class="nav-item nav-link active" href="ContactUs.php">Contact Us <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="Profile.php">Profile</a>';
+                }else{
+                    echo '<a class="nav-item nav-link" href="MyNote.php">My Notes</a>
+      <a class="nav-item nav-link" href="AboutUs.php">About Us</a>
+      <a class="nav-item nav-link" href="ContactUs.php">Contact Us</a>
+      <a class="nav-item nav-link active" href="Profile.php">Profile  <span class="sr-only">(current)</span></a>';
+                }
+        ?>
     </div>
        <div class=" my-2 mr-2  my-lg-0">
     <a class="btn btn-outline-danger my-2 mx-1 my-sm-0" type="submit" id="SignOut" href="/config/logout.php?logout=1" >Sign Out</a>

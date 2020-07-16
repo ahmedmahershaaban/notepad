b4088b2ccf93b0e0856133c2e1dd71f3cc1267e4
ahmedmahershaaban <ahmedmaher13579@outlook.com>
2020-07-16 05:55:09 +0200

    Config Database for the first time with GearHost

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = @mysqli_connect('localhost','notepad','NOTEpad100%','notepad');
+$link = @mysqli_connect('den1.mysql6.gear.host','notepad','Dq30529~~hB4','notepad');
 
 if(mysqli_connect_errno()){
         die("<p style='color:red;'>WE Couldn't Connect to the database table . MYSQL Number: " . mysqli_connect_errno() . " Description : " . mysqli_connect_error() . "</p>");
